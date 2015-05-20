d00e947b263fd19d1a450eae737e7cc51f3e186c
Imre Nagi <imre@icehousecorp.com>
2015-05-20 07:22:59 +0700

    Add dialog for adding new card

--- app/src/main/java/com/imrenagi/greflashcard/EditorActivity.java
+++ app/src/main/java/com/imrenagi/greflashcard/EditorActivity.java
@@ -1,16 +1,23 @@
 package com.imrenagi.greflashcard;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class EditorActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
+    private Toolbar toolbar;
+    private EditText nameEditText;
+    private Spinner typeSpinner;
+    private EditText meaningEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,9 @@ public class EditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editor);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        nameEditText = (EditText) findViewById(R.id.name);
+        typeSpinner = (Spinner) findViewById(R.id.type);
+        meaningEditText = (EditText) findViewById(R.id.meaning);
 
         setSupportActionBar(toolbar);
 
@@ -36,9 +46,58 @@ public class EditorActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_finish) {
+
+            if (!nameEditText.getText().toString().isEmpty()
+                    && !meaningEditText.getText().toString().isEmpty()) {
+                //todo save db here
+                showSuccessDialog();
+            } else {
+                showFailedDialog();
+            }
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showSuccessDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(getString(R.string.card_saved));
+        alertDialogBuilder
+                .setMessage(getString(R.string.add_card_success_message))
+                .setCancelable(false)
+                .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                        nameEditText.setText("");
+                        meaningEditText.setText("");
+                    }
+                })
+                .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                        finish();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void showFailedDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(getString(R.string.failed_title));
+        alertDialogBuilder
+                .setMessage(getString(R.string.add_card_failed_message))
+                .setCancelable(false)
+                .setPositiveButton(getString(R.string.oke), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
